168f3f85ceb4b2b088b214571a14b04f08fad225
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-07-05 14:22:18 -0300

    COMMIT some updates

--- ROCKETQ/src/route.js
+++ ROCKETQ/src/route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const QuestionController = require("./controllers/QuestionController");
 
 const route = express.Router();
 
@@ -6,8 +7,6 @@ route.get("/", (req, res) => res.render("index"));
 route.get("/room", (req, res) => res.render("room"));
 route.get("/create-pass", (req, res) => res.render("create-pass"));
 
-route.post("/room/:room/:question/:action", (req, res) =>
-    res.render("exemplo", { req })
-);
+route.post("/room/:room/:question/:action", QuestionController.index);
 
 module.exports = route;
--- ROCKETQ/src/server.js
+++ ROCKETQ/src/server.js
@@ -10,6 +10,8 @@ server.set("view engine", "ejs");
 
 server.set("views", path.join(__dirname, "views"));
 
+server.use(express.urlencoded({ extended: true }));
+
 server.use(route);
 
 server.listen(3000, () => console.log("Rodando....."));
